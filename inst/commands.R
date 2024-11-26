e03b4806ba54b51279d246610241dfbcd4f5aee8
Desilvestro, Valentino <Valentino.Desilvestro@sympany.ch>
2024-11-26 20:55:04 +0100

    roxygen

--- inst/commands.R
+++ inst/commands.R
@@ -2,3 +2,18 @@
 
 # create package in an existing folder
 usethis::create_package(here::here())
+
+# create documentation
+roxygen2::roxygenise()
+
+# to set up the testing infrastructure.
+usethis::use_testthat()
+
+# to create a test file.
+usethis::use_test("function_name")
+usethis::use_test() # on current open file
+
+# run all package tests
+devtools::test()
+
+
